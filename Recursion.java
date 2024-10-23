cd907447bf6c424df542c44c1aad19d569533afc
shreyesh93 <shreyesh.rane93@gmail.com>
2024-10-23 20:50:54 +0530

    Power and Hanoi Tower using recursion

--- Recursion.java
+++ Recursion.java
@@ -4,31 +4,72 @@ public class Recursion {
 
    //1. Fibo using recursion 
 
-    public static void  fun1(int a,int b,int n){
+    // public static void  fun1(int a,int b,int n){
 
-        if(n==0){
+    //     if(n==0){
             
-            return;
-        }
-       else{
+    //         return;
+    //     }
+    //    else{
         
-        int c = a+b;
-        System.out.print(c);
-        System.out.print(" ");
+    //     int c = a+b;
+    //     System.out.print(c);
+    //     System.out.print(" ");
 
-        fun1(b,c,n-1);}  // will work as a=b, b = c also as we are getting 3,4,5... and so on terms we are doing n-1
-        }
+    //     fun1(b,c,n-1);}  // will work as a=b, b = c also as we are getting 3,4,5... and so on terms we are doing n-1
+    //     }
         
     
-    public static void main(String[] args) {
+    // public static void main(String[] args) {
         
-        int a =0;
-        int b =1;
-        int n = 7;
-        System.out.println("Fibonacci series till"+" "+n+" "+"is:");
-        System.out.print(a+" "+b+" ");
+    //     int a =0;
+    //     int b =1;
+    //     int n = 7;
+    //     System.out.println("Fibonacci series till"+" "+n+" "+"is:");
+    //     System.out.print(a+" "+b+" ");
         
-        fun1(a,b,n-2);  // n =5 and we are already printting 2 terms(0 and 1) , therefore n - 2
+    //     fun1(a,b,n-2);  // n =5 and we are already printting 2 terms(0 and 1) , therefore n - 2
   
-    }
+    // }
+
+    //2 . Power using Recursion (stack height = logn)
+
+        // public static int pof(int x, int n){
+        //     if(n==0){
+        //         return 1;
+        //     }
+        //     if(x ==0){
+        //         return 0;
+        //     }
+        //     if(n%2==0){
+        //         return pof(x, n/2)*pof(x, n/2);
+        //     }
+        //     else{
+        //         return pof(x, n/2)*pof(x, n/2)*x;
+        //     }
+        // }
+        // public static void main(String[] args) {
+        //     int x = 2, n = 5;
+        //     int powa = pof(x, n);
+        //     System.out.println(powa);
+        // }
+
+    //3. Tower of Hanoi using Recursion
+    
+    // public static void toh(int n,String src, String helper, String dest){
+    //     if(n==1){
+    //         System.out.println("Transfer disk "+ n + " from "+src+" to "+dest);
+    //         return;
+    //     }
+    //     toh(n-1, src, dest, helper);
+    //     System.out.println("Transfer disk "+ n + " from "+src+" to "+dest);
+    //     toh(n-1, helper, src, dest);
+    // }
+
+
+    // public static void main(String[] args) {
+    //     int n = 3;
+    //     toh(n, "S", "H", "D");
+        
+    // }
 }
